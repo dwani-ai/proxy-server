b5d23185499f6fd1865d3eb30552f14e0dc1eb97
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-09-23 15:30:38 +0200

    hello-world

---
+++ latest/src/server/hello_world.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+app = FastAPI()
+
+@app.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url="/docs")
+
+@app.get("/greet")
+async def greet():
+    return {"message": "Welcome to FastAPI!"}
